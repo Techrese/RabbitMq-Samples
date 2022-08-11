008bac02b706971b2392a77d9814f3b7f1a7a4f3
TechnologyResearcher <tim.clinckemalie@hotmail.com>
2022-08-11 15:46:25 +0200

    Added send event message

---
+++ RabbitMq-Samples/Hello-World/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("hello", false, false, false, null);
+
+        string message = "Hello World";
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish("", "hello", null, body);
+
+        Console.WriteLine($"sent message {message}");
+    }
+}
+
+Console.WriteLine("press enter to exit");
+Console.ReadLine();
---
+++ RabbitMq-Samples/Hello-World/Send.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Hello_World
+{
+    internal class Send
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    string message = "Hello World";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", "hello", null, body);
+
+                    Console.WriteLine($"snet message {message}");
+                }
+            }
+
+            Console.WriteLine("press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
