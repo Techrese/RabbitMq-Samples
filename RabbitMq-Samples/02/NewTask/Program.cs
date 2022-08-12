79d7d5d05282864aacb6c24004fd98478272149d
TechnologyResearcher <tim.clinckemalie@hotmail.com>
2022-08-12 11:24:10 +0200

    Added changes to send and receive testing mulltiple consumers

--- RabbitMq-Samples/02/NewTask/Program.cs
+++ RabbitMq-Samples/02/NewTask/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("hello", false, false, false, null);
+
+        var message = GetMessage(args);
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish("", "hello", null, body);
+
+        Console.WriteLine($"sent message {message}");
+    }
+}
+
+Console.WriteLine("press enter to exit");
+Console.ReadLine();
+
+
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+}
--- RabbitMq-Samples/02/Worker/Program.cs
+++ RabbitMq-Samples/02/Worker/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("hello", false, false, false, null);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Received: {message}");
+
+            int dots = message.Split('.').Length - 1;
+            Thread.Sleep(dots * 1000);
+
+            Console.WriteLine("done");
+        };
+
+        channel.BasicConsume("hello", true, consumer);
+
+        Console.WriteLine("press enter to exit");
+        Console.ReadLine();
+    }
+}
